b9400ed999589b63be6dd0f1c410dfc698813cf8
babjo <kd980311@naver.com>
2014-05-04 21:46:38 +0900

    add test mvc config class

---
+++ src/main/java/org/a_sply/porter/config/TestMVCConfig.java
@@ -0,0 +1,21 @@
+package org.a_sply.porter.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Petri Kainulainen
+ */
+@Configuration
+@ComponentScan(basePackages = { "org.a_sply.porter.controller"})
+@EnableWebMvc
+public class TestMVCConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
